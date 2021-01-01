00282078ca75c4d5a22110f68ae0bec75231d300
Franklin Collin Tamboto <tamboto2000@gmail.com>
2021-01-01 20:40:35 +0700

    Add reader

---
+++ reader.go
@@ -0,0 +1,33 @@
+package json5extract
+
+import (
+	"bytes"
+	"io"
+	"strings"
+)
+
+type reader interface {
+	ReadByte() (byte, error)
+	UnreadByte() error
+}
+
+func readFromBytes(byts []byte) reader {
+	buff := bytes.NewReader(byts)
+
+	return buff
+}
+
+func readFromString(str string) reader {
+	rdr := strings.NewReader(str)
+	return rdr
+}
+
+func readFromReader(r io.Reader) (reader, error) {
+	init := make([]byte, 0)
+	buff := bytes.NewBuffer(init)
+	if _, err := buff.ReadFrom(r); err != nil {
+		return nil, err
+	}
+
+	return buff, nil
+}
