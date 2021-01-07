6a3f1289e77e4bcd4066802e39713a79126b49d7
Franklin Collin Tamboto <tamboto2000@gmail.com>
2021-01-07 19:03:12 +0700

    Add methods to extract from io.Reader and string

--- json5extract.go
+++ json5extract.go
@@ -1,6 +1,9 @@
 package json5extract
 
-import "os"
+import (
+	"io"
+	"os"
+)
 
 // FromFile extract JSON5 strings from a file in path
 func FromFile(path string) ([]*JSON5, error) {
@@ -20,5 +23,21 @@ func FromFile(path string) ([]*JSON5, error) {
 // FromBytes extract JSON5 strings from array of bytes
 func FromBytes(byts []byte) ([]*JSON5, error) {
 	r := readFromBytes(byts)
+	return parseAll(r)
+}
+
+// FromReader extract JSON5 strings from io.Reader
+func FromReader(rdr io.Reader) ([]*JSON5, error) {
+	r, err := readFromReader(rdr)
+	if err != nil {
+		return nil, err
+	}
+
+	return parseAll(r)
+}
+
+// FromString extract JSON5 strings from string
+func FromString(str string) ([]*JSON5, error) {
+	r := readFromString(str)
 	return parseAll(r)
 }
