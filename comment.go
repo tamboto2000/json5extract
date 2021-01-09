f0714b7acdcb2e3bc4e15bdbcf58bdc0b566760b
Franklin Collin Tamboto <tamboto2000@gmail.com>
2021-01-09 19:32:13 +0700

    Add methods to detect and ignore comment

---
+++ comment.go
@@ -0,0 +1,67 @@
+package json5extract
+
+import "io"
+
+func parseComment(r reader) (int, error) {
+	char, _, err := r.ReadRune()
+	if err != nil {
+		if err == io.EOF {
+			return 0, ErrInvalidFormat
+		}
+
+		return 0, err
+	}
+
+	// single line comment
+	if char == '/' {
+		for {
+			char, _, err := r.ReadRune()
+			if err != nil {
+				if err == io.EOF {
+					return 0, ErrInvalidFormat
+				}
+
+				return 0, err
+			}
+
+			if char == '\r' || char == '\n' {
+				break
+			}
+		}
+
+		return commInline, nil
+	}
+
+	// multi line comment
+	if char == '*' {
+		for {
+			char, _, err := r.ReadRune()
+			if err != nil {
+				if err == io.EOF {
+					return 0, ErrInvalidFormat
+				}
+
+				return 0, err
+			}
+
+			if char == '*' {
+				char, _, err := r.ReadRune()
+				if err != nil {
+					if err == io.EOF {
+						return 0, ErrInvalidFormat
+					}
+
+					return 0, err
+				}
+
+				if char == '/' {
+					break
+				}
+			}
+		}
+
+		return commMultiLine, nil
+	}
+
+	return 0, ErrInvalidFormat
+}
